6f6efd7f664efdc0f67d551785849b974c74874f
Dallin Christensen <dallin.christensen@gmail.com>
2016-05-28 01:09:17 -0600

    Change server config to make Redhat Openshift happy

--- sports-roster-server.js
+++ sports-roster-server.js
@@ -38,8 +38,8 @@ app.get('/api/:league/:teamid/:playernumber', function (request, response) {
 	response.json(playersWithNumber);
 });
 
-var server = app.listen(8081, function () {
-	var host = server.address().address
-	var port = server.address().port
-	console.log("Sports Roster server listening at http://localhost:" + port)
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server = app.listen(server_port, server_ip_address, function () {
+	console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
